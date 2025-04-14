0027f737a28972b516e1d640a0e62f59d0e85e41
João Gabriel Pereira Gomes <joaogabrielpg13@gmail.com>
2025-04-14 14:45:38 -0300

    feature/Adicionando lista de Reprodução

--- manga-sound-player/src/main/java/br/com/mangarosa/collections/ListaReproducao.java
+++ manga-sound-player/src/main/java/br/com/mangarosa/collections/ListaReproducao.java
@@ -1,5 +1,34 @@
 package br.com.mangarosa.collections;
 
 public class ListaReproducao {
-
+    private String nome;
+    private ListaEncadeada musicas;
+    public ListaReproducao(String nome) {
+        this.nome = nome;
+        this.musicas = new ListaEncadeada();
+    }
+    public String getNome() {
+        return nome;
+    }
+    public ListaEncadeada getMusicas() {
+        return musicas;
+    }
+    public void adicionarMusica(Musica musica) {
+        musicas.append(musica);
+    }
+    public boolean removerMusica(int pos) {
+        return musicas.remove(pos);
+    }
+    public void listarMusicas() {
+        for (int i = 0; i < musicas.size(); i++) {
+            Musica m = (Musica) musicas.get(i);
+            System.out.println((i + 1) + ". " + m.getNome());
+        }
+    }
+    public boolean estaVazia() {
+        return musicas.isEmpty();
+    }
 }
+
+
+
